b3d590bc22b2436289343f006d96dd8aa9a8b49a
Lauren Oliver <laurenoliver17@gmail.com>
2017-07-22 17:12:45 -0500

    Add pseudocode and encrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,35 @@
+# Encrypt Method:
+# Define a method called "encrypt", and set a parameter
+# Assign the index a start value of 0
+# Set variable to empty string 
+# Set a variable string for the entire alphabet
+# Include loop method to cycle through entire parameter length, 
+# increasing by 1 each time.
+# Set a new variable that captures the conversion of the alphabet
+# to integers.
+# Include if/else statement for final integer '25' to cycle back
+# to 0.
+# Define empty string variable is increasing to the letter variable
+# based on the new index that was increased by 1.
+# Print the return to the console 
+
+def encrypt(password)
+  index = 0
+  new_password = ""
+  letters = "abcdefghijklmnopqrstuvwxyz" 
+
+  while index < password.length
+   integers = letters.index(password[index]) #convert letters to integers
+    if integers == 25
+      restart_index = 0
+    else 
+      restart_index = integers + 1
+    end  
+  new_password += letters[restart_index]
+  index += 1
+  end
+  new_password
+end
+
+encrypt("zebra")
+
