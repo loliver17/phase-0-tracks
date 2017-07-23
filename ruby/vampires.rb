60c603b4d761f0f10a507c0fc53ca3c854db3610
Lauren Oliver <laurenoliver17@gmail.com>
2017-07-23 18:55:18 -0500

    Adjust indent for readability

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,24 +4,21 @@
 def new_employee
 	puts "What is your name?"
 		name = gets.chomp
-		p name
 	puts "How old are you?"
 		age = gets.chomp
 		#convert answer to integer
 		age = age.to_i
-		p age
 	puts "What year were you born?"
 		birth_year = gets.chomp
 		#convert answer to integer
 		birth_year = birth_year.to_i
-		p birth_year
 	#compare age to birth_year and assign boolean value 
-	if (2016 - birth_year) == age
-	  boolean_age = true
-	 else
-	   boolean_age = false
-	 end
-	p boolean_age
+		if (2016 - birth_year) == age
+		  boolean_age = true
+		 else
+		   boolean_age = false
+		 end
+		p boolean_age
 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 		garlic_bread = gets.chomp
 		#convert answer to boolean"
@@ -100,16 +97,11 @@ def new_employee
 	  vampire_status = "Probably not a vampire." if boolean_age && (garlic_bread || insurance) == true
 	  vampire_status = "Probably a vampire." if !boolean_age && (!garlic_bread || !insurance) == true
 	  vampire_status = "Almost certainly a vampire." if !boolean_age && !garlic_bread && !insurance == true
-	  #not working for Tu Fang
-	  #vampire_status = "Definitely a vampire." if name == ("Drake Cula || Tu Fang")
 	  vampire_status = "Definitely a vampire." if name == "Tu Fang"
 	  vampire_status = "Definitely a vampire." if name == "Drake Cula"
 	  vampire_status = "Probably a vampire." if allergies_answer == true
 	  vampire_status = "Results inconclusive." if vampire_status == nil
-	  #return vampire_status
-	  #if vampire_status == nil
-	   # vampire_status = "Results inconclusive"
-	  #end
+	  
 	  
 	  p vampire_status
 
