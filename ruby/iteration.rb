97f558de5a48cf0f08c1c96390a65ef483b196d8
Lauren Oliver <laurenoliver17@gmail.com>
2017-07-29 13:42:39 -0500

    Add status message before and after block is run

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -3,7 +3,9 @@ def names
 	name_1 = gets.chomp
 	puts "what is your name?"
 	name_2 = gets.chomp
+	puts "The names are: #{name_1} and #{name_2}"
 	yield(name_1, name_2)
+	puts "The block was successful if you see the message: Hi #{name_1} and #{name_2}!"
 end
 
 names { |name_1, name_2| puts "Hi #{name_1} and #{name_2}!" }
