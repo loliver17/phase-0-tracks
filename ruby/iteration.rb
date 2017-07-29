ae334d43d2b40159f08d57de014ce6c1d5e51edb
Lauren Oliver <laurenoliver17@gmail.com>
2017-07-29 13:58:35 -0500

    Add code to iterate through an array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,3 +9,21 @@ def names
 end
 
 names { |name_1, name_2| puts "Hi #{name_1} and #{name_2}!" }
+
+pets = ['Sacha', 'Zeke', 'Mack', 'Zoe', 'Lou']
+favorite_pets = {'Sacha':3, 'Zeke': 2, 'Mack': 5, 'Zoe': 4, 'Lou': 1}
+
+pets.each do |pet|
+	puts pet
+end
+
+puts "After .each call:"
+p pets
+
+pets.map! do |pet|
+	puts pet
+	pets.reverse
+end
+
+puts "After .map! call:"
+p pets	
