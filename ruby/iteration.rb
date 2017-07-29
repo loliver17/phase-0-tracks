54e42029cfdd4056de732d63ac29edab10bc3a3c
Lauren Oliver <laurenoliver17@gmail.com>
2017-07-29 14:32:27 -0500

    Update .each and .map! iteration

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -11,7 +11,11 @@ end
 names { |name_1, name_2| puts "Hi #{name_1} and #{name_2}!" }
 
 pets = ['Sacha', 'Zeke', 'Mack', 'Zoe', 'Lou']
-favorite_pets = {'Sacha':3, 'Zeke': 2, 'Mack': 5, 'Zoe': 4, 'Lou': 1}
+favorite_pets = {Sacha: '3rd', Zeke: '2nd', Mack: '5th', Zoe: '4th', Lou: '1st'}
+
+#.each on pets array
+puts "Original data:"
+p pets
 
 pets.each do |pet|
 	puts pet
@@ -20,10 +24,26 @@ end
 puts "After .each call:"
 p pets
 
+#.map! on pets array
+puts "Original data:"
+p pets
+
 pets.map! do |pet|
 	puts pet
-	pets.reverse
+	pet.reverse
 end
 
 puts "After .map! call:"
 p pets	
+
+#.each on the hash data structure
+puts "Original data:" 
+p favorite_pets
+
+favorite_pets.each do |pet, rank|
+	puts "#{pet} is ranked #{rank}"
+end
+
+puts "After .each call:"
+p favorite_pets
+
