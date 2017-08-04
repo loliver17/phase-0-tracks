af7d2de6197a96b7fe93c79fc84a152b1b707723
Lauren Oliver <laurenoliver17@gmail.com>
2017-08-04 16:01:33 -0500

    Add class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end		
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -29,6 +33,7 @@ class Puppy
   	puts "wags tail"
   end	
 
+
 end #end of puppy
 
 #---------------------------------------
@@ -40,6 +45,47 @@ lou.speak(5)
 lou.roll_over
 lou.dog_years(3)
 lou.wag
+
+#------------------------------------------
+
+class Rahm_E
+
+  def initialize
+  	puts "Re-elected to office."
+  end
+
+  def shake_hands(x)
+  	puts "shake hands " * x 
+  end		
+
+  def cover_ups(scandal)
+  	puts "Covers up #{scandal}." 
+  end
+	
+end	
+
+#------------------------------------------
+#Driver code
+
+mayor = Rahm_E.new
+mayor.shake_hands(5)
+mayor.cover_ups("police shooting")
+
+daley_plaza = []
+mayor_counter = 0
+until mayor_counter == 50
+	mayor_counter = mayor_counter + 1
+	new_mayor = ("new mayor" + mayor_counter.to_s)
+	new_mayor = Rahm_E.new 
+	daley_plaza.push(new_mayor)
+end	
+puts daley_plaza
+
+daley_plaza.each {|mayor| mayor.shake_hands(1)}
+daley_plaza.each {|mayor| mayor.cover_ups("another police shooting")}
+
+
+
 
 
 
