9ab6aae7fba254bde34245f3a49ad07812c6d96c
Lauren Oliver <laurenoliver17@gmail.com>
2017-08-04 14:58:42 -0500

    Write 4 instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,41 @@ class Puppy
     toy
   end
 
-end
+  def speak(x)
+  	index = x
+  	until index == 0
+  		puts "Woof!"
+  	index -= 1	
+  	end	
+  end
+
+  #def bark(x) Fast way to repeat something
+  #	puts "bowwow " * x 
+  #end	
+
+  def roll_over()
+  	puts "rolls over"
+  end
+  
+  def dog_years(x)
+  	p x * 7
+  end	
+
+  def wag()
+  	puts "wags tail"
+  end	
+
+end #end of puppy
+
+#---------------------------------------
+#Driver code
+
+lou = Puppy.new
+lou.fetch("ball")
+lou.speak(5)
+lou.roll_over
+lou.dog_years(3)
+lou.wag
+
+
 
