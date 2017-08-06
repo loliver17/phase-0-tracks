26b4de7c4ccaedb8766f3b8c87388d49bf3711e7
Lauren Oliver <laurenoliver17@gmail.com>
2017-08-05 21:43:46 -0500

    Refactor attribute instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,8 @@
 class Santa
 
+	attr_reader :ethnicity
+	attr_accessor :reindeer_ranking, :gender, :age
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end	
@@ -13,13 +16,70 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
-			"Comet" "Cupid", "Donner", "Blitzen"]
+			"Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end
+
+#setter methods
+
+	def get_mad_at(reindeer_name)
+		reindeer_ranking.insert(-1, reindeer_ranking.delete_at(reindeer_ranking.index(reindeer_name)))
+		#new_reindeer_ranking = reindeer_ranking.index(reindeer_name)
+		#@reindeer_ranking.index(reindeer_name) #= @reindeer_ranking.index[-1]
+	end	
+
+	def celebrate_birthday
+		age.next
+	end
+
+	def gender(new_gender)
+		gender = new_gender
+	end	
+
+#getter methods
+	#def age
+	#	@age
+	#end
+	
+	#def ethnicity
+	#	@ethnicity
+	#end			
+
 			
 end	
 
 #Driver Code
-george = Santa.new
-george.speak
-george.eat_milk_and_cookies("oatmeal raisin")
+#santa = Santa.new
+#santa.speak
+#santa.eat_milk_and_cookies("oatmeal raisin")
+
+#names = ["Santa_Claus", "Sinter_Klaus", "Joulupukki", "Father_Christmas", "Kris_Kringle"]
+santas = []
+
+#names.each do |name|
+#	santas << Santa.new(name)
+#end
+
+santas << Santa.new("transgender", "asian")
+santas << Santa.new("cisgender", "white")
+santas << Santa.new("female", "native")
+santas << Santa.new("male", "black")
+santas << Santa.new("agender", "hispanic")
+#p santas
+p santas[0]
+p santas[0].age
+p santas[0].ethnicity
+p santas[0].gender("bigender")
+p santas[0].get_mad_at("Dasher")
+p santas[0].celebrate_birthday
+p santas[0]
+
+
+
+
+
+
+
+
+
+
