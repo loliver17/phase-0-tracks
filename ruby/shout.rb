762ce598d12a3bf66f25df5fbfde9bccbfbbc01d
Lauren Oliver <laurenoliver17@gmail.com>
2017-08-06 16:59:43 -0500

    Add mixin module to 2 classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,35 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+#module Shout
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :("
+#	end
 	
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
-	end		
+#	def self.yelling_happily(words)
+#		words + "!!!" + " :)"
+#	end		
+#end
+
+#Driver code
+#p Shout.yell_angrily("Why would you do that?")
+#p Shout.yelling_happily("I'm so glad you're here")
+
+module Shout
+	def yell(words)
+		puts "#{words}!!!!"
+	end
+end
+
+class Cops
+	include Shout
+end
+
+class Parents
+	include Shout
 end
 
 #Driver code
-p Shout.yell_angrily("Why would you do that?")
-p Shout.yelling_happily("I'm so glad you're here")
+
+cop = Cops.new
+cop.yell("Move your car")
+
+parent = Parents.new
+parent.yell("Go to bed")
